b4105fa72d11f941c8e1bc162b6c05c63446ef80
Evander Oktapian <evandertambunan@gmail.com>
2023-09-19 21:18:52 +0700

    4 Method

--- routers/api/article.router.js
+++ routers/api/article.router.js
@@ -2,7 +2,18 @@ const express = require("express")
 const articleRouter = express.Router()
 
 articleRouter.get("/", (req,res)=> {
-       res.send("ini endpoint get article")
+       res.send("method GET")
+})
+articleRouter.post("/", (req,res)=> {
+       res.send("method POST")
+})
+articleRouter.put("/:id", (req,res)=> {
+       const {id} = req.params
+       res.send(`method PUT dengan id : ${id}`)
+})
+articleRouter.delete("/:id", (req,res)=> {
+       const {id} = req.params
+       res.send(`method DELETE dengan id: ${id}`)
 })
 
 
